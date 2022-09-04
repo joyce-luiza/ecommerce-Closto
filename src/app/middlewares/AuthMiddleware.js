466d9875795999533a4ffdbd02fdbfc1859ceb3b
joyce-luiza <joyceluizabrito@gmail.com>
2022-09-03 21:11:12 -0300

    authConfig import

--- src/app/middlewares/AuthMiddleware.js
+++ src/app/middlewares/AuthMiddleware.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import { promisify } from "util";
-import auth from "../../config/auth";
+import authConfig from "../../config/auth";
 
 export default async (req, res, next) => {
     const authHeader = req.headers.authorization;
