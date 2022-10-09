374e0366eef61c968f0c342c293705b2ed0b6f4a
Joyce Luiza <61090216+joyce-luiza@users.noreply.github.com>
2022-10-09 14:45:08 -0300

    Update 20221001215221-create-orders.js

--- server/src/database/migrations/20221001215221-create-orders.js
+++ server/src/database/migrations/20221001215221-create-orders.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.createTable("products", {
+        await queryInterface.createTable("orders", {
             id: {
                 type: Sequelize.INTEGER,
                 autoIncrement: true,
